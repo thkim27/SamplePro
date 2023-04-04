15412bed03e4f671bb7520a980ac0aa0f94e5454
thkim <tahyo11@naver.com>
2023-04-04 16:41:56 +0900

    Hello Test2

--- GitTest3/src/HelloTest.java
+++ GitTest3/src/HelloTest.java
@@ -3,6 +3,8 @@ public class HelloTest {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Test.");
+		System.out.println("Hello Test.");
+		System.out.println("Hello Test.");
 
 	}
 
